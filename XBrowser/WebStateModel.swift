332b2322f31b76d598de8ab322df1f826ea8cdf6
Bùi Đỗ Trung Trực <ptvh.dbs02@gmail.com>
2022-01-06 08:43:33 +0700

    code convention

--- XBrowser/WebStateModel.swift
+++ XBrowser/WebStateModel.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 class WebStateModel {
-  static let shared = WebStateModel()
-
-  @Published var canGoBack = false
-  @Published var canGoForward = false
-  @Published var submittedUrl: String?
-  @Published var currentUrl: String?
+    static let shared = WebStateModel()
+    
+    @Published var canGoBack = false
+    @Published var canGoForward = false
+    @Published var submittedUrl: String?
+    @Published var currentUrl: String?
 }
--- XBrowser/WebViewController.swift
+++ XBrowser/WebViewController.swift
@@ -10,56 +10,45 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController, WKUIDelegate {
-
-  var viewModel: WebViewModel!
-
-  var webView: WKWebView!
-
-  var cancellables = Set<AnyCancellable>()
-
-  override func loadView() {
-    let webConfiguration = WKWebViewConfiguration()
-    webView = WKWebView(frame: .zero, configuration: webConfiguration)
-    webView.uiDelegate = self
-    webView.allowsBackForwardNavigationGestures = true
-    view = webView
-
-    webView.publisher(for: \WKWebView.canGoBack)
-      .assign(to: \.canGoBack, on: viewModel)
-      .store(in: &cancellables)
-
-    webView.publisher(for: \WKWebView.canGoForward)
-      .assign(to: \.canGoForward, on: viewModel)
-      .store(in: &cancellables)
-
-    webView.publisher(for: \WKWebView.url)
-      .map { $0?.absoluteString }
-      .assign(to: \.url, on: viewModel)
-      .store(in: &cancellables)
-
-    viewModel.$url
-      .sink(receiveValue: { [self] url in
-        guard let url = url,
-          let _URL = URL(string: url)
-        else { return }
-        let request = URLRequest(url: _URL)
-        webView.load(request)
-      })
-      .store(in: &cancellables)
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
-  /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    var viewModel: WebViewModel!
+    
+    var webView: WKWebView!
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        view = webView
+        
+        webView.publisher(for: \WKWebView.canGoBack)
+            .assign(to: \.canGoBack, on: viewModel)
+            .store(in: &cancellables)
+        
+        webView.publisher(for: \WKWebView.canGoForward)
+            .assign(to: \.canGoForward, on: viewModel)
+            .store(in: &cancellables)
+        
+        webView.publisher(for: \WKWebView.url)
+            .map { $0?.absoluteString }
+            .assign(to: \.url, on: viewModel)
+            .store(in: &cancellables)
+        
+        viewModel.$url
+            .sink(receiveValue: { [self] url in
+                guard let url = url,
+                      let _URL = URL(string: url)
+                else { return }
+                let request = URLRequest(url: _URL)
+                webView.load(request)
+            })
+            .store(in: &cancellables)
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
